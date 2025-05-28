ccea09b784aa8240f82bcafd48af25d8b33d2e65
Brian Meshell <bmeshell@mitre.org>
2025-05-28 06:13:05 -0400

    verified working with a single instance of Practitioner

--- src/FhirInstanceUploader.java
+++ src/FhirInstanceUploader.java
@@ -1,5 +1,10 @@
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import com.google.gson.JsonObject;
@@ -8,7 +13,7 @@ import com.google.gson.JsonParser;
 public class FhirInstanceUploader {
 
     public void uploadAllJsonInFolder() {
-        final String FhirServerBaseUrl = "";
+        final String FhirServerBaseUrl = "https://gw.interop.community/bmeshelltest/open/";
         File folder = new File("src/upload");
         if (folder.exists() && folder.isDirectory()) {
             File[] files = folder.listFiles();
@@ -18,9 +23,33 @@ public class FhirInstanceUploader {
                         try {
                             String fileContent = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
                             JsonObject jsonObject = JsonParser.parseString(fileContent).getAsJsonObject();
+
+                            String myResourceType = jsonObject.get("resourceType").toString();
+                            String myId = jsonObject.get("id").toString();
+                            String myUri = FhirServerBaseUrl + myResourceType.substring(1, myResourceType.length() - 1) + "/" + myId.substring(1, myId.length() - 1);
+
+                            HttpRequest request = HttpRequest.newBuilder()
+                                    .uri(new URI(myUri))
+                                    .headers("Content-Type", "application/json")
+                                    .PUT(HttpRequest.BodyPublishers.ofString(fileContent))
+                                    .build();
+
+                            HttpClient client = HttpClient.newHttpClient();
+
+                            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
                             System.out.println("hi");
-                        } catch (IOException e) {
+                        }
+                        catch (IOException e) {
                             System.out.println("Error reading file: " + file.getName());
+                            e.printStackTrace();
+                        }
+                        catch (URISyntaxException e) {
+                            System.out.println("URI syntax exception: " + file.getName());
+                            e.printStackTrace();
+                        }
+                        catch (InterruptedException e) {
+                            System.out.println("Interrupted exception: " + file.getName());
                             e.printStackTrace();
                         }
                     }
